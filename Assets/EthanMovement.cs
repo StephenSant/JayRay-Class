ba21abeb5e1945750c01ff011ce331590a7dd7a1
Stephen <42423953+StephenSant@users.noreply.github.com>
2019-05-17 15:36:47 +1000

    I DONT WANNA

---
+++ Assets/EthanMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthanMovement : MonoBehaviour
+{
+    public Animator animator;
+    public float walkSpeed = 1, runSpeed = 2;
+
+    Vector2 movement;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movement = new Vector2(input.x*2, input.y*2);
+        }
+        else
+        {
+        movement = new Vector2(input.x, input.y);
+        }
+        animator.SetFloat("Horizontal", movement.x); 
+        animator.SetFloat("Vertical", movement.y); 
+        animator.SetFloat("Speed", movement.y); 
+    }
+}
